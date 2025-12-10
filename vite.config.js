a0c51c0fecd0fcbfc19d58772880ac65989eadb7
Shyam Prasath <shyam182004@gmail.com>
2025-12-10 22:42:45 +0530

    Initial commit: push all project files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+// Get __dirname in ESM
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = path.dirname(__filename);
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 5173
+        port: 5173,
     },
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "src")
-        }
-    }
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
 });
